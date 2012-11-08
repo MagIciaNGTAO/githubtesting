8f88c4f9ae68756196ade293a8fdf61b6f5ed72e
craigmingtaozhang <mingtaoz@cmu.edu>
2012-11-07 20:04:19 -0500

    test

--- Githubtesting/src/test.java
+++ Githubtesting/src/test.java
@@ -10,5 +10,6 @@ public class test {
   public static void main(String[] args){
     System.out.println("we are testing github");
     System.out.println("with little progress");
+    System.out.println("with little...");
   }
 }
