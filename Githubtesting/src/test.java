14089ec5a76d79c99f68f91b4a28fc5695fe9c2e
Mingtao Zhang <mingtaoz@cmu.edu>
2012-11-08 10:51:20 -0500

    Update Githubtesting/src/test.java

--- Githubtesting/src/test.java
+++ Githubtesting/src/test.java
@@ -9,8 +9,5 @@
 public class test {
   public static void main(String[] args){
     System.out.println("Change for testing");
-    System.out.println("we are testing github");
-    System.out.println("with little progress");
-    System.out.println("with little...");
   }
 }
