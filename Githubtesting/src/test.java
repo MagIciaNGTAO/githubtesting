3e7383adc47864a19c5b03eb316a0ff11d8712fe
yiweil <lyw900422@gmail.com>
2012-11-08 11:19:44 -0500

    change

--- Githubtesting/src/test.java
+++ Githubtesting/src/test.java
@@ -9,5 +9,7 @@
 public class test {
   public static void main(String[] args){
     System.out.println("Change for testing");
+    System.out.println("Change for testing");
+    System.out.println("Change for testing");
   }
 }
