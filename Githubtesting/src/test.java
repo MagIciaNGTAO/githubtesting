a0926167db606d66894406f5048b1849562212c8
yiweil <lyw900422@gmail.com>
2012-11-08 11:30:44 -0500

    M

--- Githubtesting/src/test.java
+++ Githubtesting/src/test.java
@@ -10,6 +10,6 @@ public class test {
   public static void main(String[] args){
     System.out.println("Change for testing");
     System.out.println("Change for testing");
-    System.out.println("Change for testing");
+    System.out.println("Change for MMM");
   }
 }
