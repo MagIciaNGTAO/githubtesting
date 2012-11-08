3e656d4e8e9d7eaf6f4df7e14a7a2205a235d018
craigmingtaozhang <mingtaoz@cmu.edu>
2012-11-08 10:52:38 -0500

    To testbranch

--- Githubtesting/src/test.java
+++ Githubtesting/src/test.java
@@ -8,6 +8,10 @@
  */
 public class test {
   public static void main(String[] args){
+    System.out.println("Change for testing");
+    System.out.println("we are testing github");
+    System.out.println("we are testing github");
+    System.out.println("Change for testing");
     System.out.println("we are testing github");
     System.out.println("with little progress");
     System.out.println("with little...");
